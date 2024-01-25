02118ffc0741934a48be507409a4120cdbacb21c
Tekin Evrim Ozmermer <evrimozmermer@hotmail.com>
2024-01-25 22:14:21 +0200

    further adjustments

--- models/__init__.py
+++ models/__init__.py
@@ -2,10 +2,10 @@ from .seg_models_pytorch import SMP
 
 def load(cfg):
     model = SMP(
-        num_classes=cfg.model.smp.out_layer_size,
-        in_channels=cfg.model.smp.in_channels,
-        backbone=cfg.model.smp.backbone,
-        pretrained=cfg.model.smp.pretrained_weights,
-        model_name=cfg.model.smp.arch,
+        num_classes=cfg.model.out_layer_size,
+        in_channels=cfg.model.in_channels,
+        backbone=cfg.model.backbone,
+        pretrained=cfg.model.pretrained_weights,
+        model_name=cfg.model.arch,
     )
     return model
